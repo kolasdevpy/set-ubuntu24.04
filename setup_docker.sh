6a72319117c571ba2757149a9dd677ada7d6cebb
Artsiom Kolas <61182975+kolasdevpy@users.noreply.github.com>
2026-04-15 15:45:12 +0300

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -36,6 +36,6 @@ sudo docker --version
 sudo docker compose version
 
 echo "------------------------------"
-echo "⚠️  Reload the terminal"
 echo "⚠️  run: sudo usermod -aG docker admin"
 echo "⚠️  run: newgrp docker"
+echo "⚠️  Reload the terminal"
